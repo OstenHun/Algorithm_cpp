b9ae8c48cf23425eeaaae467f6526047b0397dd9
DongHun <81342336+OstenHun@users.noreply.github.com>
2024-07-05 01:20:23 +0900

    Update A／B.cc

--- 백준/Bronze/1008. A／B/A／B.cc
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -26,3 +26,14 @@ int main() {
 
     return 0;
 }
+
+// setprecision과 cout.precision의 차이
+// setprecision은 함수 호출 형태로 사용하고, cout.precision은 멤버 함수 호출 형태로 사용.
+// setprecision은 #include <iomanip>가 필요하며, 스트림 조작자. cout.precision은 스트림 객체의 멤버 함수.
+
+// 동작 방식:
+// fixed 조작자와 함께 사용하면, 둘 다 소수점 이하 자릿수를 설정.
+// fixed 조작자 없이 사용하면, 둘 다 전체 유효 자릿수를 설정.
+-> 따라서 소수점 아래 자릿수에 대해 정확하게 접근하기 위해서는 fixed 조작자와 함께 사용하여 소수점 이하 자릿수를 설정해야한다.
+    cout.precision(자릿수 + 1)
+    cout << fixed;
