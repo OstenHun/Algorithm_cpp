1dc6ab23a6f0832956888dd5ba4ae996de45fe5d
DongHun <81342336+OstenHun@users.noreply.github.com>
2024-07-05 00:51:50 +0900

    Update A／B.cc

--- 백준/Bronze/1008. A／B/A／B.cc
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -10,3 +10,19 @@ int main() {
     cout << divide;
     return 0;
 }
+
+// chatgpt가 추천한 풀이.
+-> fixed, setprecision을 활용해 소수점 이하 자릿수를 고정해(고정소수점) 출력의 정확성을 올린다.
+    
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double A, B;
+    cin >> A >> B;
+
+    // 나눈 결과를 소수점 10자리까지 출력
+    cout << fixed << setprecision(10) << A / B << endl;
+
+    return 0;
+}
