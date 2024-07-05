45f740e22154b70399e08e8c3a2845a25a09c848
DongHun <81342336+OstenHun@users.noreply.github.com>
2024-07-06 00:03:05 +0900

    [Bronze II] Title: 단어의 개수, Time: 1012 ms, Memory: 12108 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> split(string input, string delimiter) {
+    vector<string> ret;
+    long long pos = 0;
+    string token = "";
+    while((pos = input.find(delimiter)) != string::npos) {
+        token = input.substr(0, pos);
+        ret.push_back(token);
+        input.erase(0, pos + delimiter.length());
+    }
+    ret.push_back(input);
+    return ret;
+}
+
+int main() {
+    string s;
+    int count = 0;
+    getline(cin, s);
+    vector<string> a = split(s, " ");
+    for(string b : a) {
+        if (b != "") {
+            count++;
+        }
+    }
+    cout << count;
+}
