dfbb2d3f4015a402007ef7c190a6b2a2b7d4fe47
DongHun <81342336+OstenHun@users.noreply.github.com>
2024-07-06 01:57:58 +0900

    Update 피보나치 수.cc

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.cc
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.cc
@@ -1,3 +1,4 @@
+//Recursion 이용 >> 이는 시간복잡도 O(2^n) 이므로 비효율적임.
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -7,6 +8,29 @@ int Fibo(int n) {
     
     int a = 0, b = 1, c;
     for(int i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
+int n = 0;
+int main() {
+    cin >> n;
+    cout << Fibo(n);
+}
+
+//반복문 사용.
+#include <bits/stdc++.h>
+using namespace std;
+
+int Fibo(int n) {
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+
+    int a = 0, b = 1, c;
+    for (int i = 2; i <= n; i++) {
         c = a + b;
         a = b;
         b = c;
