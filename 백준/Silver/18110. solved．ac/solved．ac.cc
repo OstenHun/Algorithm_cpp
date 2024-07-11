97785e76ad23dce2de779fd09c8ad401d74a2235
DongHun <81342336+OstenHun@users.noreply.github.com>
2024-07-12 03:07:48 +0900

    Update solved．ac.cc

--- 백준/Silver/18110. solved．ac/solved．ac.cc
+++ 백준/Silver/18110. solved．ac/solved．ac.cc
@@ -28,3 +28,32 @@ int main() {
 
     cout << ret;
 }
+
+// 이 코드도 존재.
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, num, ex, ret;
+vector<int> inputs;
+double sum;
+int main() {
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> num;
+        inputs.push_back(num);
+    } 
+    sort(inputs.begin(), inputs.end());
+    ex = round(n * 0.15);
+
+    // 여기가 다름 포인트
+    for (int i = ex; i < n - ex; i++) {
+        sum += inputs[i];
+    }
+
+    ret = round(sum/(n - ex * 2));
+    
+    if(n == 0)
+        ret = 0;
+
+    cout << ret;
+}
