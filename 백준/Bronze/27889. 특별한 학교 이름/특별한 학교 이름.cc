5eaf41958b9438232ca17ffaca932c1e19c21eab
DongHun <81342336+OstenHun@users.noreply.github.com>
2024-07-12 03:11:58 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string str;
+int main() {
+    cin >> str;
+    if (str == "NLCS")
+        cout << "North London Collegiate School";
+    else if (str == "BHA")
+        cout << "Branksome Hall Asia";
+    else if (str == "KIS")
+        cout << "Korea International School";
+    else if (str == "SJA")
+        cout << "St. Johnsbury Academy";
+
+    return 0;
+}
