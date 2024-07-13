0c4d0f4c366f47d6ba9341d61370b9718808c2aa
DongHun <81342336+OstenHun@users.noreply.github.com>
2024-07-14 03:02:01 +0900

    Update 랜선 자르기.cc

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -1,3 +1,32 @@
+// 선형 탐색의 경우 시간이 초과되기 때문에 이분 탐색을 생각해야함.
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, k, num, mn;
+double ret;
+vector<int> inputs;
+int main() {
+    cin >> k >> n;
+    for (int i = 0; i < k; i++) {
+        cin >> num;
+        inputs.push_back(num);
+    }
+
+    while (true) {
+        mn++;
+        for (auto i : inputs) {
+            ret += i / mn;
+        }
+        if (ret < n)
+            break;
+        ret = 0;
+    }
+    cout << mn-1;
+    return 0;
+}
+
+
+// 이분 탐색을 활용한 풀이
 #include <bits/stdc++.h>
 using namespace std;
 
