5b1c90b00663bf3a8639da4b1f90447fa927a567
DongHun <81342336+OstenHun@users.noreply.github.com>
2024-07-24 01:58:27 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -3,12 +3,22 @@ using namespace std;
 
 int l;
 string str;
-long long ret;
+long long ret, R = 1;
+int M = 1234567891;
 int main() {
     cin >> l >> str;
+    
+    // 50점 받은 풀이.
+    // for (int i = 0; i < str.size(); i++) {
+    //     ret += ((str[i]-96) * pow(31, i));
+    // }
+
     for (int i = 0; i < str.size(); i++) {
-        ret += ((int)str[i]-96) * pow(31, i);
+        ret += ((str[i] - 96) * R) % M;
+        ret %= M;
+        R = (R * 31) % M;   
     }
+        
     cout << ret;
     return 0;
 }
