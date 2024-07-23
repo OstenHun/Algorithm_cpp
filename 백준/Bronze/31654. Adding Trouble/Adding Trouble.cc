86a5b0983f8aa95479af60f58e4230238e0826d3
DongHun <81342336+OstenHun@users.noreply.github.com>
2024-07-24 00:51:04 +0900

    [Bronze V] Title: Adding Trouble, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31654. Adding Trouble/Adding Trouble.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b, c;
+int main() {
+    cin >> a >> b >> c;
+    if (a + b == c)
+        cout << "correct!";
+    else   
+        cout << "wrong!";
+}
