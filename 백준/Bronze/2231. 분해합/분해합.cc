6eb8f1e19e0aad7e1fa86e7cca612455a1655eb1
DongHun <81342336+OstenHun@users.noreply.github.com>
2024-07-30 18:38:18 +0900

    Update 분해합.cc

--- 백준/Bronze/2231. 분해합/분해합.cc
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -5,6 +5,8 @@ int n, ret;
 int main() {
     cin>> n;
 
+    // tmp를 쓰는 이유
+    // -> i 값에 변화를 줘서는 안된다. i의 값을 직접 건드리지 않기 위해 tmp 변수 활용.
     for (int i = n; 1 <= i; i--) {
         int sum = i;
         int tmp = i;
