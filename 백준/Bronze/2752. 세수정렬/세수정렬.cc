e4591486436ae44636a33a87d7ceea042e20e07d
DongHun <81342336+OstenHun@users.noreply.github.com>
2024-08-04 21:13:03 +0900

    Update 세수정렬.cc

--- 백준/Bronze/2752. 세수정렬/세수정렬.cc
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -1,3 +1,39 @@
+// 수학적 풀이
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int a, b, c; // 입력
+  cin >> a >> b >> c;
+  int d, e, f; // 크기 순으로 출력할 세 수
+  d = min({a,b,c});
+  f = max({a,b,c});
+  e = a+b+c-d-f;
+  cout << d << ' ' << e << ' ' << f;
+}
+
+// sort 사용하기
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  int arr[4];
+  for(int i=0;i<3;i++){
+    cin>>arr[i];
+  }
+  sort(arr, arr+3);
+  for(int i=0;i<3;i++){
+    cout<<arr[i]<<" ";
+  }
+}
+
+// 내 풀이
 #include <bits/stdc++.h>
 using namespace std;
 
