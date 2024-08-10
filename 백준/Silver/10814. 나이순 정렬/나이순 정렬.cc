47063b57dbfcec855fda5da6ff87ca2a1909a909
DongHun <81342336+OstenHun@users.noreply.github.com>
2024-08-11 01:40:42 +0900

    Update 나이순 정렬.cc

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -1,3 +1,5 @@
+// 내가 푼 풀이. 기본 sort()의 성질을 활용해 tuple을 만들고 기존의 입력 순서를 넣어줄 변수 num을 가지고 
+// sort() 한 후 나이 순으로 정렬 된 후 만약 나이가 같다면 입력 순서인 num을 통해 정렬하도록 설계했다.
 // #include <bits/stdc++.h>
 // using namespace std;
 
@@ -20,6 +22,8 @@
 //     return 0;
 // }
 
+// stable_sort와 람다 비교 함수를 통해 나이만을 통해 정렬 후 만약 나이가 같다면 입력 순서 그대로 정렬하는 코드
+// Gpt 4-o 를 통해 얻은 결과이다.
 #include <bits/stdc++.h>
 using namespace std;
 
