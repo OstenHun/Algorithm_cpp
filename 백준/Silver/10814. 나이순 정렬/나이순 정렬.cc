7c18b280b490a13aa5eadc2ea823d9a373c9242f
DongHun <81342336+OstenHun@users.noreply.github.com>
2024-08-11 01:36:14 +0900

    [Silver V] Title: 나이순 정렬, Time: 68 ms, Memory: 7900 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -1,20 +1,43 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int n, age, num;
+// string name;
+// int main() {
+//     cin >> n;
+//     vector<tuple<int, int, string>> inputs;
+//     for (int i = 0; i < n; i++) {
+//         cin >> age >> name;
+//         num++;
+//         inputs.emplace_back(age, num, name);
+//     }
+
+//     sort(inputs.begin(), inputs.end());
+//     for (auto i : inputs) {
+//         cout << get<0>(i) << " " << get<2>(i) << '\n';
+//     }
+
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, age, num;
+int n, age;
 string name;
 int main() {
     cin >> n;
-    vector<tuple<int, int, string>> inputs;
+    vector<pair<int, string>> members(n);
     for (int i = 0; i < n; i++) {
-        cin >> age >> name;
-        num++;
-        inputs.emplace_back(age, num, name);
+        cin >> members[i].first >> members[i].second;
     }
 
-    sort(inputs.begin(), inputs.end());
-    for (auto i : inputs) {
-        cout << get<0>(i) << " " << get<2>(i) << '\n';
+    stable_sort(members.begin(), members.end(),[](const pair<int, string>& a, const pair<int, string>& b) {
+        return a.first < b.first;
+    });
+
+    for (auto i : members) {
+        cout << i.first << " " << i.second << '\n';
     }
 
     return 0;
