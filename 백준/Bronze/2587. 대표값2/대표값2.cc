4e6b6a9f7e5a3c8b2b9c6d8f2a29d627cc2517ca
DongHun <81342336+OstenHun@users.noreply.github.com>
2024-08-12 00:19:24 +0900

    Update 대표값2.cc

--- 백준/Bronze/2587. 대표값2/대표값2.cc
+++ 백준/Bronze/2587. 대표값2/대표값2.cc
@@ -18,3 +18,24 @@ int main() {
     cout << median << '\n';
     return 0;
 }   
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int num[5], total;
+
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+    
+  for(int i = 0; i < 5; i++) cin >> num[i];
+    
+  // 평균
+  for(int i = 0; i < 5; i++) total += num[i];
+  cout << total / 5 << "\n";
+    
+  // 중앙값
+  sort(num, num + 5);
+  cout << num[2];
+}
