58eeddaa0112039b2fc47f5f5d68fa304d2b3047
DongHun <81342336+OstenHun@users.noreply.github.com>
2024-08-13 17:54:21 +0900

    Update 빠른 A＋B.cc

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -12,3 +12,22 @@ int main() {
         cout << a + b << '\n';
     }
 }
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+    
+  int T;
+  cin >> T;
+    
+  for(int t = 0; t < T; t++){
+    int A, B;
+    cin >> A >> B;
+      
+    cout << A + B << "\n";
+  }
+}
