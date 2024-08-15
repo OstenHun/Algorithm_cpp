7d9bd3aff403f4424fef207f7af7f40e75931770
DongHun <81342336+OstenHun@users.noreply.github.com>
2024-08-16 02:53:25 +0900

    Update 최댓값.cc

--- 백준/Bronze/2562. 최댓값/최댓값.cc
+++ 백준/Bronze/2562. 최댓값/최댓값.cc
@@ -17,3 +17,40 @@ int main() {
     cout << indx + 1 << '\n';
     return 0;
 }
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, maxvalue, maxindex;
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+    
+  for(int i = 1; i < 10; i++){
+    cin >> n;
+    // 전역변수의 초기값은 0이므로 바로 비교 가능합니다.
+    if(maxvalue < n){
+      maxvalue = n;
+      maxindex = i;
+    }
+  }
+  cout << maxvalue << "\n" << maxindex;
+}
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[9];
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  for(int i = 0; i < 9; i++) cin >> a[i];
+  cout << *max_element(a, a+9) << '\n';
+  cout << max_element(a, a+9) - a + 1;
+}
+
+/*
+max_element 함수를 이용한 풀이
+*/
