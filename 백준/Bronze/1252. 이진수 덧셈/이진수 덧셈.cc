4017c22158be142bed50838df1554cb6e09bc0bd
DongHun <81342336+OstenHun@users.noreply.github.com>
2024-08-21 03:04:04 +0900

    [Bronze II] Title: 이진수 덧셈, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/1252. 이진수 덧셈/이진수 덧셈.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string a, b;
+int main() {
+    cin >> a >> b;
+    
+    bitset<81> A(a);
+    bitset<81> B(b);
+
+    while (B.any()) {
+        bitset<81> c = A & B;
+        A = A ^ B;
+        B = c << 1;
+    }
+
+    string ret = A.to_string();
+    ret.erase(0, ret.find_first_not_of('0'));
+    if (ret.empty()) 
+        ret = "0";
+    
+    cout << ret << '\n';
+}
