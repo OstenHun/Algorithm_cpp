e0d99d001aab8560ecd950dcbe4c67ecf4bbfed1
DongHun <81342336+OstenHun@users.noreply.github.com>
2024-08-29 01:39:50 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long i, sum;
+int main() {
+    for (int j = 0; j < 5; j++) {
+        cin >> i;
+        sum += i;
+    }
+
+    cout << sum << '\n';
+}
