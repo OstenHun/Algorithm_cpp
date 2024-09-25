418adff8e7558323c6f16da7b851f17d3f39d9c3
DongHun <81342336+OstenHun@users.noreply.github.com>
2024-09-26 01:58:46 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 4400 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -1,47 +1,19 @@
-// // 최악의 경우 메모리 사용이 많음 
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// int n, num;
-// vector<int> nms;
-// int main() {
-//     cin >> n;
-//     for (int i = 0; i < n; i++) {
-//         cin >> num;
-//         nms.push_back(num);
-//     }
-//     sort(nms.begin(), nms.end());
-
-//     for(int i : nms) {
-//         cout << i << '\n';
-//     }
-
-//     return 0;
-
-// }
-
-#include <iostream>
-#include <vector>
-
+#include <bits/stdc++.h>
 using namespace std;
 
+int n, num;
 int main() {
-    int n;
     cin >> n;
 
-    // 정수의 범위가 1부터 10000까지이므로 10001 크기의 배열 생성
-    vector<int> count(10001, 0);
+    vector<int> cnt(10001, 0);
 
-    // 입력된 숫자의 개수를 카운팅
-    for (int i = 0; i < n; i++) {
-        int num;
+    for(int i = 0; i < n; i++) {
         cin >> num;
-        count[num]++;
+        cnt[num]++;
     }
 
-    // 정렬된 숫자 출력
     for (int i = 1; i <= 10000; i++) {
-        for (int j = 0; j < count[i]; j++) {
+        for (int j = 0; j < cnt[i]; j++) {
             cout << i << '\n';
         }
     }
