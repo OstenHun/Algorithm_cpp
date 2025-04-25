61ffc889895e05c26632fa1608857f2773c399a8
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-04-26 03:19:54 +0900

    [Bronze II] Title: Game Night, Time: 12 ms, Memory: 5120 KB -BaekjoonHub

--- 백준/Bronze/33253. Game Night/Game Night.cc
+++ 백준/Bronze/33253. Game Night/Game Night.cc
@@ -1,29 +1,61 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int n;
+// int main() {
+//     int a[26] = {};
+//     cin >> n;
+
+//     string old, nw;
+//     cin >> old >> nw;
+
+//     for (const auto &w : nw) {
+//         a[w-'a']++;
+//     }
+
+//     for (const auto &w : old) {
+//         if (a[w-'a']==0) continue;
+//         a[w-'a']--;
+//     }
+
+//     int sum = 0;
+//     for (int i = 0; i<26; i++) {
+//         sum+= a[i];
+//     }
+
+//     cout << sum;
+    
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
 int main() {
-    int a[26] = {};
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
     cin >> n;
 
-    string old, nw;
-    cin >> old >> nw;
+    string old_pw, new_pw;
+    cin >> old_pw >> new_pw;
 
-    for (const auto &w : nw) {
-        a[w-'a']++;
+    // array 설정하기
+    array<int, 26> fre{};
+
+    for (char c : new_pw) {
+        fre[c-'a']++;
+    }   
+
+    for (char c : old_pw) {
+        if (fre[c - 'a'] > 0) {
+            fre[c-'a']--;
+        }
     }
 
-    for (const auto &w : old) {
-        if (a[w-'a']==0) continue;
-        a[w-'a']--;
-    }
+    // 남은 글자 수 합산하기 -> 배열 내 모든 수 합하는 STL 알고리즘.
+    int ans = accumulate(fre.begin(), fre.end(), 0);
 
-    int sum = 0;
-    for (int i = 0; i<26; i++) {
-        sum+= a[i];
-    }
-    
-    cout << sum;
-    
-
+    cout << ans << '\n';
+    return 0;
 }
