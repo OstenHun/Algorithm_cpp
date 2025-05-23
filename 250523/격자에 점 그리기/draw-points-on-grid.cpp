3f7780ba39ba76a9358c75c8accbfb212e695151
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-05-23 11:05:25 +0900

    Update draw-points-on-grid.cpp

--- 250523/격자에 점 그리기/draw-points-on-grid.cpp
+++ 250523/격자에 점 그리기/draw-points-on-grid.cpp
@@ -16,12 +16,20 @@ int main() {
         }
     }
 
-    int cnt = 1;
+    // int cnt = 1;
+    // for (int i = 0; i < m; i++) {
+    //     int r, c;
+    //     cin >> r >> c;
+    //     mat[r-1][c-1] = cnt++;
+    // }
+
+    // 이게 변수 하나를 덜 쓸 수 있는 방법이다.
     for (int i = 0; i < m; i++) {
         int r, c;
         cin >> r >> c;
-        mat[r-1][c-1] = cnt++;
+        mat[r-1][c-1] = i+1;
     }
+    
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
