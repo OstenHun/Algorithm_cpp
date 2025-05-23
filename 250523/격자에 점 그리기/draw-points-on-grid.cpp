0b7a018427c20ed58dcfffb64563029ebb69e723
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-05-23 11:03:08 +0900

    Title: 격자에 점 그리기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250523/격자에 점 그리기/draw-points-on-grid.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    int** mat = new int* [n];
+    for (int i = 0; i < n; i++) {
+        mat[i] = new int[n];
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            mat[i][j] = 0;
+        }
+    }
+
+    int cnt = 1;
+    for (int i = 0; i < m; i++) {
+        int r, c;
+        cin >> r >> c;
+        mat[r-1][c-1] = cnt++;
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << mat[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    for (int i = 0; i < n; i++) {
+        delete[] mat[i];
+    }
+
+    delete[] mat;
+    return 0;
+}
