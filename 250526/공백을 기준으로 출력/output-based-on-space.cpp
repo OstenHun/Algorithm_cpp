948106b4f909788e305a2ca4bd838995a3b153ea
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-05-26 11:16:14 +0900

    Update output-based-on-space.cpp

--- 250526/공백을 기준으로 출력/output-based-on-space.cpp
+++ 250526/공백을 기준으로 출력/output-based-on-space.cpp
@@ -1,3 +1,5 @@
+
+// 1번 풀이
 #include <iostream>
 #include <string>
 using namespace std;
@@ -22,5 +24,28 @@ int main() {
     s1.append(s2);
 
     cout << s1;
+    return 0;
+}
+
+// 2번 풀이
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s1, s2;
+    getline(cin, s1);
+    getline(cin, s2);
+
+    for (int i = 0; i < s1.length(); i++) {
+        if (s1[i] != ' ')
+            cout << s1[i];
+    }
+
+    for (int i = 0; i < s2.length(); i++) {
+        if (s2[i] != ' ')
+            cout << s2[i];
+    }
+
     return 0;
 }
