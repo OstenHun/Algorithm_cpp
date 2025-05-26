b90b78a884a190175318a023df0917eaeb11ae17
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-05-26 13:35:38 +0900

    Title: 단어로 구분하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250526/단어로 구분하기/separate-words-with-words.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string mat[10];
+    
+    for (int i = 0; i < 10; i++) {
+        cin >> mat[i];
+
+        cout << mat[i] << '\n';
+    }
+    return 0;
+}
