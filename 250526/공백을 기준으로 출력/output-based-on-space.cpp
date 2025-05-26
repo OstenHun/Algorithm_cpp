0d94f70092be67c93292209364c0afb0a2cab952
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-05-26 11:07:30 +0900

    Title: 공백을 기준으로 출력, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250526/공백을 기준으로 출력/output-based-on-space.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void replaceAll(string &str, const string& from, const string& to) {
+    if (from.empty()) return;
+    size_t start_pos = 0;
+    while((start_pos = str.find(from, start_pos)) != string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length();
+    }
+}
+
+int main() {
+    string s1, s2;
+    getline(cin, s1);
+    getline(cin, s2);
+
+    replaceAll(s1, " ", "");
+    replaceAll(s2, " ", "");
+
+    s1.append(s2);
+
+    cout << s1;
+    return 0;
+}
