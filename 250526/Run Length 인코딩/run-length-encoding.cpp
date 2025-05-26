4a8c13c543238abd3937217b61000a5e6a85c423
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-05-26 17:02:12 +0900

    Update run-length-encoding.cpp

--- 250526/Run Length 인코딩/run-length-encoding.cpp
+++ 250526/Run Length 인코딩/run-length-encoding.cpp
@@ -1,3 +1,4 @@
+// 문자열 한 칸씩 이동하며 뒤의 문자와 같을 경우, 다를 경우를 나눠서 개수 세기
 #include <iostream>
 #include <string>
 
@@ -25,4 +26,38 @@ int main() {
     cout << ans;
 
     return 0;
+}
+
+/* 
+현재 카운트 하는 문자를 변수에 저장해서 
+문자열의 문자가 달라질 때 
+현재의 문자를 갱신하며 개수 카운트
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+string A;
+
+int main() {
+    cin >> A;
+    string encoded = "";
+
+    char cur_char = A[0];
+    int num_char = 1;
+    for (int i = 1; i < A.length(); i++) {
+        if (A[i] == cur_char) {
+            num_char++;
+        } else {
+            encoded += cur_char;
+            encoded += to_string(num_char);
+            cur_char = A[i];
+            num_char = 1;
+        } 
+    }
+    
+    encoded += cur_char;
+    encoded += to_string(num_char);
+
+    cout << encoded.length() << '\n' << encoded;
 }
