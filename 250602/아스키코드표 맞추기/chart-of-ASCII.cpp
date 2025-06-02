8731361a4809d326d2ca572060844c44574579e5
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-06-02 16:05:58 +0900

    Title: 아스키코드표 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250602/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 0 ; i < 5; i++) {
+        int chk;
+        cin >> chk;
+        cout << (char) chk << ' ';
+    }
+    return 0;
+}
