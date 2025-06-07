1bed8620497a3db576e748ce2bd1dd14f8d9358b
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-06-07 16:59:15 +0900

    Title: 함수를 이용한 온전수 판별, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250607/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b;
+
+bool IsOn(int x) {
+    if (x % 2 == 0) return false;
+    if (x % 10 == 5) return false;
+    if (x % 3 == 0 && x % 9 != 0) return false;
+    return true; 
+}
+
+int main() {
+    cin >> a >> b;
+
+    int cnt = 0;
+    for (int i = a; i <= b; i++) {
+        if (IsOn(i)) cnt++; 
+    }
+    
+    cout << cnt;
+
+    return 0;
+}
