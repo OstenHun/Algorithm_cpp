c5f9b7928fec1b29b1dbdad6ba4cc8dcd4313a2e
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-06-07 14:05:29 +0900

    Title: 함수를 이용한 윤년 판별, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250607/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
+++ 250607/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.cpp
@@ -7,8 +7,10 @@ int y;
 bool LeapYear(int x) {
     if (x % 100 == 0 && x % 400 != 0)
         return false;
-    if (x % 4 == 0) 
-        return true;
+    if (x % 4 != 0) 
+        return false;
+
+    return true;
 }
 
 int main() {
