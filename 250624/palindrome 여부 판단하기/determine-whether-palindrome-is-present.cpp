de569f97b2bf11d43369c50b59c7417780d67ee1
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-06-24 00:22:47 +0900

    Update determine-whether-palindrome-is-present.cpp

--- 250624/palindrome 여부 판단하기/determine-whether-palindrome-is-present.cpp
+++ 250624/palindrome 여부 판단하기/determine-whether-palindrome-is-present.cpp
@@ -1,10 +1,11 @@
+// 내 풀이
+
 #include <iostream>
 #include <string>
 
 using namespace std;
 
 string A;
-
 
 void IsPalindrome(string &A) {
     bool chk = true;
@@ -22,5 +23,30 @@ int main() {
     // Please write your code here.
 
     IsPalindrome(A);
+    return 0;
+}
+
+
+// 해설 풀이.
+#incldue <iostream>
+#include <string>
+
+using namespace std;
+
+string str;
+
+bool Palindrome(string &s) {
+    for (int i = 0; i < (int) s.size(); i++) {
+        if (s[i] != s[(int) s.size() - i - 1]) return false;
+        return true;
+    }
+}
+
+int main() {
+    cin >> str;
+
+    if (Palindrome(str)) cout << "Yes";
+    else cout << "No";
+
     return 0;
 }
