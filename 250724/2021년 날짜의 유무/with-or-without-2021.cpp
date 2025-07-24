a7b24d1b291a20ac350f08b1bd6cac8a2d25bf8d
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-07-24 22:39:12 +0900

    Title: 2021년 날짜의 유무, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250724/2021년 날짜의 유무/with-or-without-2021.cpp
+++ 250724/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -7,7 +7,7 @@ int M, D;
 bool IsDate(int m, int d) {
     int dates[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-    if (1 <= d && d <= dates[m-1]) return true;
+    if (m <= 12 && 1 <= d && d <= dates[m-1]) return true;
     return false;
 }
 
