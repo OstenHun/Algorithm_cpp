c2e533174c9a6c1f82fc1b671b88830124d3d04e
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-07-24 22:09:11 +0900

    Update to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp

--- 250724/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
+++ 250724/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
@@ -1,3 +1,4 @@
+// 내 풀이
 #include <iostream>
 
 using namespace std;
@@ -29,6 +30,52 @@ int main() {
 
     if (chk(n1, n2)) cout << "Yes";
     else cout << "No";
+
+    return 0;
+}
+
+// 해설
+#inlcude <iostream>
+using namespace std;
+
+#define MAX_N 100
+
+int n1, n2;
+int a[MAX_N], b[MAX_N];
+
+bool IsSame(int n) {
+    for (int i = 0; i < n2; i++) {
+        if (a[i+n] != b[i])
+            return false;
+    }
+
+    return true;
+}
+
+bool IsSubsequence() {
+    for (int i = 0; i <= n1 - n2; i++) {
+        if (IsSame(i))
+            return true;
+    }
+
+    return false;
+}
+
+int main() {
+    cin >> n1 >> n2;
+
+    for (int i = 0; i < n1; i++) {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < n2; i++) {
+        cin >> b[i];
+    }
+
+    if (IsSubsequence())
+        cout << "Yes";
+    else
+        cout << "No";
 
     return 0;
 }
