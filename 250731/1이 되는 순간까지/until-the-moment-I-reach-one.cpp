445a0c7aa9a59c300fa2feae77415e609ca8cfbc
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-07-31 20:59:23 +0900

    Update until-the-moment-I-reach-one.cpp

--- 250731/1이 되는 순간까지/until-the-moment-I-reach-one.cpp
+++ 250731/1이 되는 순간까지/until-the-moment-I-reach-one.cpp
@@ -17,6 +17,18 @@ int F(int n) {
     return F(n/3);
 
 }
+
+// 해설 기반 -> 이게 좀 더 재귀를 잘 이용한 풀이인 것 같다.
+int GetNum(int n) {
+    if (n==1)
+        return 0;
+
+    if (n % 2 == 0)
+        return GetNum(n/2) + 1;
+    
+    return GetNum(n/3) + 1;
+}
+
 int main() {
     cin >> N;
 
