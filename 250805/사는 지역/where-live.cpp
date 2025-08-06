de23b3c4330c2be83fec551edc437bb96562dd79
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-08-06 19:58:14 +0900

    Update where-live.cpp

--- 250805/사는 지역/where-live.cpp
+++ 250805/사는 지역/where-live.cpp
@@ -41,3 +41,49 @@ int main() {
 
     return 0;
 }
+
+
+// 해설 풀이 -> 선형 정렬로 문제 상황만을 해결하는데 가장 효율적임.
+#include <iostream>
+using namespace std;
+
+const int MAXN = 10;
+
+class Person {
+    public:
+        string name;
+        string street_address;
+        string region;
+        
+        Person(string name, string street_address, string region) {
+            this->name = name;
+            this->street_address = street_address;
+            this->region = region;
+        }
+        Person(){}
+};
+
+Person people[MAXN];
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        string name, address, region;
+        cin >> name >> address >> region;
+
+        people[i] = Person(name, address, region);
+    }
+
+    int last_idx = 0;
+    for (int i = 1; i < n; i++) {
+        if (people[i].name > people[last_idx].name)
+            last_idx = i;
+    }
+
+    cout << "name " << people[last_idx].name << '\n';
+    cout << "addr " << people[last_idx].street_address << '\n';
+    cout << "city " << people[last_idx].region << '\n';
+    return 0;
+}
