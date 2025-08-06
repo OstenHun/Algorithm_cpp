27e11cd412e8b4b668b3ddaed2a5a44b56653e2e
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-08-06 21:07:47 +0900

    Update rainy-day.cpp

--- 250806/비오는 날/rainy-day.cpp
+++ 250806/비오는 날/rainy-day.cpp
@@ -53,3 +53,40 @@ int main() {
     
     return 0;
 }
+
+// 더 효율적인 풀이
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Forecast {
+    public:
+        string date, day, weather;
+        Forecast(string date, string day, string weather) {
+            this->date = date;
+            this->day = day;
+            this->weather = weather;
+        }
+};
+
+Forecast ans = Forecast("9999-99-99", "", "");
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <=n; i++) {
+        string date, day, weather;
+        cin >> date >> day >> weather;
+
+        Forecast f = Forecast(date, day, weather);
+        if (weather == "Rain")
+            if (ans.date >= f.date)
+                ans = f;
+    }
+
+    cout << ans.date << " " << ans.day << " " << ans.weather;
+
+    return 0;
+}
