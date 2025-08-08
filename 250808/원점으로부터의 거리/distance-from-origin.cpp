485f0918323ff531030587d8ff3221495a6d9ea3
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-08-08 20:54:30 +0900

    Title: 원점으로부터의 거리, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250808/원점으로부터의 거리/distance-from-origin.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int N;
+
+class Position {
+    public:
+        int x, y, num;
+
+        Position(int x, int y, int z) {
+            this->x = x;
+            this->y = y;
+            this->num = z;
+        }
+};
+
+bool cmp(const Position &a, const Position &b) {
+    return a.x+a.y < b.x+b.y;
+}
+
+vector<Position> cor;
+
+int main() {
+    cin >> N;
+    int nm = 1;
+
+    for (int i = 0; i < N; i++) {
+        int x, y;
+        cin >> x >> y;
+        if (x < 0) x = -x;
+        if (y < 0) y = -y;
+        cor.emplace_back(x, y, nm++);
+    }
+
+    sort(cor.begin(), cor.end(), cmp);
+
+    for (auto i : cor) {
+        cout << i.num << '\n';
+    }
+
+    return 0;
+}
