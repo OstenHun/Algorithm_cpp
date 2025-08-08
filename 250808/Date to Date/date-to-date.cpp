14c654584b19bf2b4b3458f29fb74a6ca933c858
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-08-08 22:51:10 +0900

    Update date-to-date.cpp

--- 250808/Date to Date/date-to-date.cpp
+++ 250808/Date to Date/date-to-date.cpp
@@ -28,3 +28,30 @@ int main() {
 
     return 0;
 }
+
+// 전체 일 수 각각 구해서 빼는 방법
+#include <iostream>
+
+using namespace std;
+
+int NumOfDays(int m, int n) {
+    int days[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int total_days = 0;
+
+    for (int i = 1; i < m; i++) 
+        total_days += days[i];
+
+    total_days += d;
+
+    return total_days;
+}
+
+int m1, d1, m2, d2;
+
+int main() {
+    cin >> m1 >> d1 >> m2 >> d2;
+
+    cout << NumOfDays(m2, d2) - NumOfDays(m1, d1) + 1;
+
+    return 0;
+}
