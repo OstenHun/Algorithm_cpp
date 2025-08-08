1ed1dae585a103679882517cadb7349cc9ce2fe8
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-08-08 22:45:31 +0900

    Title: Date to Date, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250808/Date to Date/date-to-date.cpp
+++ 250808/Date to Date/date-to-date.cpp
@@ -11,15 +11,16 @@ int main() {
     int num_of_days[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     while (1) {
+        elapsed_days++;
+
         if (m1 == m2 && d1 == d2)
             break;
 
-        elapsed_days++;
         d1++;
 
         if (d1 > num_of_days[m1]) {
             m1++;
-            d1=0;
+            d1=1;
         }
     }
 
