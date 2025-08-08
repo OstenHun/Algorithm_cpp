43ec1ba3fe40e063a6912f8256f875c63f90caae
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-08-08 17:23:57 +0900

    Title: 개인정보, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250808/개인정보/personal-info.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Person {
+    public:
+        string name;
+        int height;
+        float weight;
+
+        Person(string n, int h, float w) {
+            this->name = n;
+            this->height = h;
+            this->weight = w;
+        }
+};
+
+bool cmp_name(const Person &x, const Person &y) {
+    return x.name < y.name;
+}
+
+bool cmp_height(const Person &x, const Person &y) {
+    return x.height > y.height;
+}
+
+vector<Person> people;
+
+int main() {
+    for (int i = 0; i < 5; i++) {
+        string name;
+        int h;
+        float w;
+        cin >> name >> h >> w;
+        people.emplace_back(name, h, w);
+    }
+    sort(people.begin(), people.end(), cmp_name);
+
+    cout << "name" << '\n';
+    for (auto i: people) {
+        cout << i.name << ' ' << i.height << ' ' << i.weight << '\n';
+    }
+
+    sort(people.begin(), people.end(), cmp_height);
+
+    cout << '\n' << "height" << '\n';
+    for (auto i : people) {
+        cout << i.name << ' ' << i.height << ' ' << i.weight << '\n';
+    }
+
+    return 0;
+}
