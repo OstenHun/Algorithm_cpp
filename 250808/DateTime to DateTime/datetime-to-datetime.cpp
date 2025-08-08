c30c5eda65fb45fa72c97fdf7a11dba8eb6bff92
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-08-08 23:04:28 +0900

    Title: DateTime to DateTime, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250808/DateTime to DateTime/datetime-to-datetime.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b, c;
+int elapsed_min = 0;
+
+int main() {
+    cin >> a >> b >> c;
+
+    int day = 11;
+    int hour = 11;
+    int min = 11;
+
+    if (a == 11 && b<=11 && c<=11) {
+        elapsed_min = -1;
+    }
+
+    else {
+        while (1) {
+            if (day==a && hour == b && min == c) 
+                break;
+            
+            elapsed_min++;
+            min++;
+
+            if (min == 60) {
+                hour++;
+                min = 0;
+            }
+
+            if (hour == 24) {
+                day++;
+                hour = 0;
+            }
+        }
+    }
+
+    cout << elapsed_min;
+
+    return 0;
+}
