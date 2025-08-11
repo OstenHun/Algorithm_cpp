e39e1ed3a330c694f51fa1ad09c5578054f9bc51
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-08-11 23:27:33 +0900

    Title: 여러가지 진수변환, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250811/여러가지 진수변환/various-numeral-system-transformations.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int N;
+int B;
+int digits[20];
+
+int main() {
+    cin >> N >> B;
+    int cnt = 0;
+    while (1) {
+        if (N < 2) {
+            digits[cnt++] = N;
+            break;
+        }
+        
+        digits[cnt++] = N % B;
+        N /= B;
+    }
+
+    for (int i = cnt-1; i >= 0; i--) {
+        cout << digits[i];
+    }
+
+    return 0;
+}
