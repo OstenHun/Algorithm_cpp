9a9c039fb4b747c25b4045d88b4b0f598c07fd71
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-08-12 21:46:26 +0900

    Title: 진수 to 진수, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250812/진수 to 진수/transformation-of-number-system.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int a, b;
+string n;
+
+vector<int> digits;
+int main() {
+    cin >> a >> b;
+    cin >> n;
+
+    int num = 0;
+    int sz = n.length();
+    for (int i = 0; i < sz; i++) {
+        num = num*a + (n[i]-'0');
+    }
+
+    while(1) {
+        if (num < b) {
+            digits.push_back(num);
+            break;
+        }
+
+        digits.push_back(num%b);
+        num /= 2;
+    }
+
+    for (int i = digits.size() - 1; i >= 0; i--) {
+        cout << digits[i];
+    }
+    
+    return 0;
+}
