2a4de8bde64e4887c6ab6c65f8db912a831b57a2
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-08-13 23:52:59 +0900

    Title: 최대로 겹치는 구간, Time: 7ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 250813/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n;
+int x1[100], x2[100];
+
+vector<int> ans;
+int main() {
+    cin >> n;
+    int nm = 100;
+    vector<int> ans(200);
+
+    for (int i = 0; i < n; i++) {
+        cin >> x1[i] >> x2[i];
+        if (x1[i] < nm)
+            nm = x1[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        x1[i]+=nm;
+        x2[i]+=nm;
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = x1[i]; j < x2[i]; j++) {
+            ans[j]++;
+        }
+    }
+
+    int mx = *max_element(ans.begin(), ans.end());
+
+    cout << mx;    
+
+    return 0;
+}
