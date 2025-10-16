dbcea6abd10fa518aebc19f6ae236648ee083d56
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-10-16 10:55:30 +0900

    Title: 벌금은 누구에게, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251016/벌금은 누구에게/who-will-pay.cpp
+++ 251016/벌금은 누구에게/who-will-pay.cpp
@@ -11,14 +11,19 @@ int main() {
     cin >> n >> m >> k;
     vector<int> x(n);
 
+    bool chk = false;
     for (int i = 0; i < m; i++) {
         cin >> student;
         x[student-1]++;
         if (x[student-1]>=k) {
+            chk = true;
             cout << student << '\n';
             break;
         }
+        
     } 
+
+    if (!chk) cout << -1 << '\n';
 
     return 0;
 }
