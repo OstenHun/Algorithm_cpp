e2f42e549bfbb29863084acac1ce697f5dd2163c
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-10-30 18:30:12 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251030/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
+++ 251030/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
@@ -5,6 +5,7 @@ using namespace std;
 const int MAXN = 250;
 
 int n, k, p, t;
+bool tm[MAXN];
 int x[MAXN];
 int y[MAXN];
 
@@ -21,13 +22,16 @@ int main() {
 
     for (int i = 0; i < t; i++) {
         int tt = 0, xx = 0, yy = 0;
-        cin >> tt >> xx >> yy;
+        cin >> tt >> xx >> yy;  
+        tm[tt - 1] = true;
         x[tt - 1] = xx;
         y[tt - 1] = yy;
     }
 
     for (int i = 0; i < MAXN; i++) {
-        int curr_x = x[i]-1;
+        if (tm[i]==0) continue;
+
+        int curr_x = x[i]-1; 
         int curr_y = y[i]-1;
         if (cnt[curr_x] > 0 && cnt[curr_y] > 0) {
             cnt[curr_x]--;
@@ -35,15 +39,19 @@ int main() {
         }
         else if (cnt[curr_x] > 0) {
             cnt[curr_x]--;
-            cnt[curr_y] = k;
-            ans[curr_y] = 1;
+            if (ans[curr_y]==0) {
+                cnt[curr_y] = k;
+                ans[curr_y] = 1;
+            }
         }
         else if (cnt[curr_y] > 0) {
             cnt[curr_y]--;
-            cnt[curr_x] = k;
-            ans[curr_y] = 1;
+            if (ans[curr_x]==0) {
+                cnt[curr_x] = k;
+                ans[curr_x] = 1;
+            }
         }
-    }
+    }   
 
     for (auto i : ans) {
         if (i) cout << 1;
