2eec6a90f1c88ad3cf3d4737d2a46983d41f620f
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-11-20 23:50:19 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251120/한 가지로 열리는 자물쇠/one-way-lock.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int n;
+int a, b, c;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    cin >> n;
+    cin >> a >> b >> c;
+
+    int cnt = 0;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            for (int k = 1; k <= n; k++) {
+                if (abs(a-i)<=2 || abs(b-j)<=2 || abs(c-k)<=2) 
+                    cnt++;
+            }
+        }
+    }
+
+    cout << cnt;
+    
+    return 0;
+}
