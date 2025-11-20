6f4a8571ccbbfb5645b220187a1f8d4249ca047a
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-11-20 23:38:22 +0900

    Title: 구간 중 최대 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251120/구간 중 최대 합/max-sum-of-subarray.cpp
+++ 251120/구간 중 최대 합/max-sum-of-subarray.cpp
@@ -16,7 +16,7 @@ int main() {
     }
 
     int max_val = 0;
-    for (int i = 0; i < n-k; i++) {
+    for (int i = 0; i <= n-k; i++) {
         int sum = 0;
         for (int j = i; j < i + k; j++) {
             sum+=num[j];
