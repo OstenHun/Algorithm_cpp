5dd77fdb39328b145d34e413ff5c129f25a65174
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-12-02 00:05:00 +0900

    Title: 데이터센터의 온도 조정 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251202/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, c, g, h;
+int ta[1000], tb[1000];
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    cin >> n >> c >> g >> h;
+
+    for (int i = 0; i < n; i++) {
+        cin >> ta[i] >> tb[i];
+    }
+
+    int mn = 1000;
+    int mx = 0;
+    for (int i = 0; i < n; i++) {
+        if (ta[i] < mn) mn = ta[i];
+        if (mx < tb[i]) mx = tb[i];
+    }
+
+    int ans = 0;
+    for (int i = mn; i <= mx; i++) {
+        int sum = 0;
+        for (int j = 0; j < n; j++) {
+            if (i < ta[j]) sum+=c;
+            else if (ta[j] <= i && i <= tb[j]) sum+=g;
+            else if (tb[j] < i) sum+=h;
+        }
+        ans = max(ans, sum);
+    }
+
+    cout << ans;
+
+    return 0;
+}
