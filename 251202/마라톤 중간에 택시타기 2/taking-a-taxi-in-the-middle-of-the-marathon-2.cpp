bf3e1804adaef01ea0b8926d9001a66e12c6062d
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-12-02 21:28:02 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251202/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int n;
+int ans = INT_MAX;
+int x[100], y[100];
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> x[i] >> y[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        int dist = 0;
+        int current_x = 0, current_y = 0;
+        for (int j = 0; j < n; j++) {
+            if (j==i) continue;
+            dist += abs(current_x - x[j]) + abs(current_y + y[j]);
+            current_x = x[j];
+            current_y = y[j];
+        }
+        ans = min(ans, dist);
+    }
+
+    cout << ans;
+
+    return 0;
+}
