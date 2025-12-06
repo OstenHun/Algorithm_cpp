d3b9940f9bbcd0f1f2a020129745e55e0dbe8a56
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-12-07 00:44:07 +0900

    Title: 밭의 높이를 고르게하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251207/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.cpp
+++ 251207/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.cpp
@@ -19,7 +19,7 @@ int main() {
     }
 
     int ans = INT_MAX;
-    for (int i = 0; i < n-t; i++) {
+    for (int i = 0; i <= n-t; i++) {
         int sum = 0;
         for (int j = i; j < i+t; j++) {
             sum+=abs(h-arr[j]);
