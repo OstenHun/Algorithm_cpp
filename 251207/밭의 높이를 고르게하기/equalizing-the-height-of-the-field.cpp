97d8e439c3274cc8aabee989b30a19b4545c5896
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-12-07 00:39:59 +0900

    Title: 밭의 높이를 고르게하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251207/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <climits>
+
+using namespace std;
+
+int n, h, t;
+int arr[100];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    cin >> n >> h >> t;
+
+    for (int i = 0;i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int ans = INT_MAX;
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int j = i; j < i+t; j++) {
+            sum+=abs(h-arr[j]);
+        }
+        ans = min(ans, sum);
+    }
+
+    cout << ans;
+
+    return 0;
+}
