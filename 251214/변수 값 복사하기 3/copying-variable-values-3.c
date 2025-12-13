222d51b35f0aabab8039ffa8a25bc41346be29b3
DongHun <81342336+OstenHun@users.noreply.github.com>
2025-12-14 01:11:48 +0900

    Title: 변수 값 복사하기 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251214/변수 값 복사하기 3/copying-variable-values-3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 1;
+    int b = 5;
+    int c = 3;
+    a = c;
+    a = a+c;
+    b = b-c;
+    printf("%d\n%d\n%d\n ", a, b, c);
+
+    return 0;
+}
